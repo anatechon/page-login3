1374f0167dc107ae3344084a345954e369845515
anatechon <anagabriely.feitosa@gmail>
2025-11-21 19:53:10 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); // Impede o envio sem preencher os campos
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // URL para redirecionar
+        const instagramProfileURL = 'https://www.instagram.com/anatecch'; // Substitua pelo sua URL
+
+        // Abre o link em uma nova aba ou janela
+        window.open(instagramProfileURL, '_blank');
+    });
+});
